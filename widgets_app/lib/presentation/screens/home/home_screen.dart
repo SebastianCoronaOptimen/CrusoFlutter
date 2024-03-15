0b6a59ecaa28eeb5d6d3c8e0076b3d4297091b6b
Sebastian Corona <sebastian.corona@optimen.com.mx>
2024-03-15 11:50:00 -0600

    Rutas basicas

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -50,7 +51,11 @@ class _CustomListTile extends StatelessWidget {
       title:  Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: (){
-        //TODO: Navegar a otras pantallas
+/*         Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),)
+        ); */
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
